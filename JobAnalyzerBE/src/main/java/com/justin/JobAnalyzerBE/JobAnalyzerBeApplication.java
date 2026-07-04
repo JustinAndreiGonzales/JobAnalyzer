9858b2ea8175e46c44a7378907e477149b351d0e
JustinAndreiGonzales <jcgonzales12@up.edu.ph>
2026-07-04 13:50:46 +0800

    created springboot app

---
+++ JobAnalyzerBE/src/main/java/com/justin/JobAnalyzerBE/JobAnalyzerBeApplication.java
@@ -0,0 +1,13 @@
+package com.justin.JobAnalyzerBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAnalyzerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAnalyzerBeApplication.class, args);
+	}
+
+}
---
+++ JobAnalyzerBE/src/test/java/com/justin/JobAnalyzerBE/JobAnalyzerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justin.JobAnalyzerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAnalyzerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
